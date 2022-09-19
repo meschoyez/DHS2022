bb229072552486e3466da4a3f0208b8afad627b5
meschoyez <meschoyez@gmail.com>
2022-09-19 11:57:54 -0300

    Listener (inicial)

--- src/main/python/iua/App.py
+++ src/main/python/iua/App.py
@@ -2,18 +2,22 @@ import sys
 from antlr4 import *
 from compiladoresLexer  import compiladoresLexer
 from compiladoresParser import compiladoresParser
+from MiListener import MiListener
 
 def main(argv):
     # archivo = "input/entrada.txt"
-    archivo = "input/parentesis.txt"
+    # archivo = "input/parentesis.txt"
+    archivo = "input/aritmetica.txt"
     if len(argv) > 1 :
         archivo = argv[1]
     input = FileStream(archivo)
     lexer = compiladoresLexer(input)
     stream = CommonTokenStream(lexer)
     parser = compiladoresParser(stream)
-    tree = parser.si()
-#    print(tree.toStringTree(recog=parser))
+    miListener = MiListener()
+    parser.addParseListener(miListener)
+    tree = parser.itop()
+    print(tree.toStringTree(recog=parser))
 
 if __name__ == '__main__':
     main(sys.argv)
---
+++ src/main/python/iua/MiListener.py
@@ -0,0 +1,21 @@
+from antlr4 import *
+if __name__ is not None and "." in __name__:
+    from .compiladoresParser import compiladoresParser
+else:
+    from compiladoresParser import compiladoresParser
+
+# This class defines a complete listener for a parse tree produced by compiladoresParser.
+class MiListener(ParseTreeListener):
+
+    # Enter a parse tree produced by compiladoresParser#term.
+    def exitTerm(self, ctx:compiladoresParser.TermContext):
+        print ("Term tiene " + str(ctx.getChildCount()) + " hijos")
+        print ("Term -> text |" + ctx.getText() + "|")
+    
+    # Enter a parse tree produced by compiladoresParser#factor.
+    def enterFactor(self, ctx:compiladoresParser.FactorContext):
+        print ("Factor IN -> |" + ctx.getText() + "|")
+
+    # Exit a parse tree produced by compiladoresParser#factor.
+    def exitFactor(self, ctx:compiladoresParser.FactorContext):
+        print ("Factor OUT -> |" + ctx.getText() + "|")
